8d3a240e4ffd1566b8f11526c3ea4688ea11303e
zivyou <yzq529@qq.com>
2019-07-14 10:53:37 -0400

    update

--- app.js
+++ app.js
@@ -1,20 +1,67 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoClient = require("mongodb").MongoClient;
+const dbUrl = "mongodb://localhost:27017";
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var dbName = "myDb";
+var collectionName = "articles";
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.get('/', (req, res)=>{
-    res.render("index", {
+    return res.render("index", {
         "title": "Articles"
     });
 });
 
 
-app.get('/article/new', (req, res)=>{
-    res.render("new", {
-        "title": "add new article"
+app.get('/article/new', (req, res, next)=>{
+    mongoClient.connect(dbUrl, (err, mongo)=>{
+        if (err){
+            console.log("connect to db failed.");
+            return res.status(500).send("mongodb error!");
+        }else{
+            console.log("connect to db OK!");
+        }
+        let db = mongo.db(dbName);
+        let collection = db.collection(collectionName);
+        collection.find().toArray((err, docs)=>{
+            mongo.close();
+            return res.render("new", {
+                "allArticles": docs
+            });
+        });
+    });
+});
+
+
+app.post('/article/new', (req, res, next)=>{
+    // let article = JSON.parse(req.body);
+    console.log("posting...", req.body);
+    let article = {"title":"test article", content:"this is  a test article"};
+    mongoClient.connect(dbUrl, (err, mongo)=>{
+        if (err){
+            console.error("connect to db failed.");
+            return res.status(500).send("mongodb error!");
+        }
+        let db = mongo.db(dbName);
+        let collection = db.collection(collectionName);
+        collection.insertOne(article, (err, re)=>{
+            let v = JSON.parse(re);
+            if (v.n == 1){
+                mongo.close();
+                return res.redirect(302, "/article/new");
+            }else{
+                mongo.close();
+                return res.status(500).send("add article failed!");           
+            }
+        });
     });
 });
 
